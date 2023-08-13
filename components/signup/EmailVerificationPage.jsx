69e3bdb800e6473f8962e6bf20aa912c69931acf
hyeseung <wjoo3@naver.com>
2023-08-13 17:17:23 +0900

    Docs/todo 주석 추가

--- components/signup/EmailVerificationPage.jsx
+++ components/signup/EmailVerificationPage.jsx
@@ -2,25 +2,24 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import styled from 'styled-components/native';
 
-import CertificatePage from './CertificatePage';
 import DetailsInfoPage from './DetailsInfoPage';
 import SignupHeader from './SignupHeader';
+
+/* TODO: 전체 완료 시 삭제
+ * 이메일 입력 input 생성
+ * 인증번호 전송 button 생성
+ * 인증번호 입력 input 생성
+ * 인증완료 button 생성
+ * 인증 실패 modal 생성
+ * 인증 완료 modal 생성
+ * 사용자 소속 학교에 따라 이메일 정보 받아오기 (@ 뒷부분)
+ * 인증번호 전송 button 클릭 시 인증번호 전송
+ * 입력한 인증번호와 실제 인증번호 비교
+ */
 
 const Container = styled.View`
   flex: 1;
   align-items: center;
-`;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Text = styled.Text`
-  color: white;
-  font-weight: 500;
-`;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Btn = styled.TouchableOpacity`
-  background-color: black;
-  padding: 10px 15px;
-  padding: 10px 15px;
-  margin: auto 0;
 `;
 
 const EmailVerificationPage = () => {
@@ -28,14 +27,6 @@ const EmailVerificationPage = () => {
   return (
     <Container>
       <SignupHeader prevPage={DetailsInfoPage} />
-      {/* FIXME: 버튼과 텍스트 부분 페이지 작업 들어가면 삭제 */}
-      <Btn
-        onPress={() => {
-          navigation.navigate(CertificatePage);
-        }}
-      >
-        <Text>이메일 인증 페이지</Text>
-      </Btn>
     </Container>
   );
 };
