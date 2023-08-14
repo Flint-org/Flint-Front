d7aa63c5edc0b8e81edbbcd3e7072b42a032ffeb
hyeseung <wjoo3@naver.com>
2023-08-14 17:00:54 +0900

    Feat/버튼에 임의로 로직 적용 (다음 페이지 넘어가기)

--- components/signup/StartPage.jsx
+++ components/signup/StartPage.jsx
@@ -6,11 +6,12 @@ import { useNavigation } from '@react-navigation/native';
 import FlintLogoSvg from '../../assets/images/flint_logo.svg';
 import KakaoLogoSvg from '../../assets/images/kakao_logo.svg';
 import NaverLogoSvg from '../../assets/images/naver_logo.svg';
+import DetailsInfoPage from './DetailsInfoPage';
 
 /* TODO: 전체 완료 시 삭제
  * 로고 삽입 (O)
- * 카카오로 시작하기 버튼 생성
- * 네이버로 시작하기 버튼 생성
+ * 카카오로 시작하기 버튼 생성 (O)
+ * 네이버로 시작하기 버튼 생성 (O)
  * 카카오 버튼 로직
  * 네이버 버튼 로직
  * 회원가입 완료시 DetailsInfoPage로 넘어가기
@@ -45,17 +46,32 @@ const BtnText = styled.Text`
   font-weight: 600;
 `;
 
-const SignupPage = () => {
+const StartPage = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
       <WithLocalSvg width={135} asset={FlintLogoSvg} />
       <BtnWrap>
-        <StartBtn backgroundColor={'#fae100'}>
+        {/* FIXME: 로직 수정 필요 (우선 임의로 다음 페이지로 넘어가도록 해둠)*/}
+        <StartBtn
+          backgroundColor={'#fae100'}
+          onPress={() => {
+            navigation.navigate(DetailsInfoPage);
+          }}
+        >
           <WithLocalSvg height={22} asset={KakaoLogoSvg} />
           <BtnText color={'#381e1f'}>카카오톡으로 시작하기</BtnText>
         </StartBtn>
+        {/* FIXME: 로직 수정 필요 (우선 임의로 다음 페이지로 넘어가도록 해둠)*/}
         <StartBtn backgroundColor={'#3bac37'}>
-          <WithLocalSvg height={22} asset={NaverLogoSvg} />
+          <WithLocalSvg
+            height={22}
+            asset={NaverLogoSvg}
+            onPress={() => {
+              navigation.navigate(DetailsInfoPage);
+            }}
+          />
           <BtnText color={'#fff'}>네이버로 시작하기</BtnText>
         </StartBtn>
       </BtnWrap>
@@ -63,4 +79,4 @@ const SignupPage = () => {
   );
 };
 
-export default SignupPage;
+export default StartPage;
