6cb0ec9542a6414cc4822a523abe8bdb56ccc90e
hyeseung <wjoo3@naver.com>
2023-08-14 16:53:25 +0900

    Feat/플린트 로고 삽입

--- components/signup/StartPage.jsx
+++ components/signup/StartPage.jsx
@@ -1,37 +1,34 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { css } from 'react';
 import styled from 'styled-components/native';
+import { WithLocalSvg } from 'react-native-svg';
+import { useNavigation } from '@react-navigation/native';
 
-import DetailsInfoPage from './DetailsInfoPage';
+import FlintLogoSvg from '../../assets/images/flint_logo.svg';
+import KakaoLogoSvg from '../../assets/images/kakao_logo.svg';
+import NaverLogoSvg from '../../assets/images/naver_logo.svg';
+
+/* TODO: 전체 완료 시 삭제
+ * 로고 삽입 (O)
+ * 카카오로 시작하기 버튼 생성
+ * 네이버로 시작하기 버튼 생성
+ * 카카오 버튼 로직
+ * 네이버 버튼 로직
+ * 회원가입 완료시 DetailsInfoPage로 넘어가기
+ */
 
 const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-`;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Text = styled.Text`
-  color: white;
-  font-weight: 500;
-`;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Btn = styled.TouchableOpacity`
-  background-color: black;
-  padding: 10px 15px;
+  padding: 120px 30px 0;
+  background-color: white;
+  gap: 155;
 `;
 
 const SignupPage = () => {
-  const navigation = useNavigation();
   return (
     <Container>
-      {/* FIXME: 버튼과 텍스트 부분 페이지 작업 들어가면 삭제 */}
-      <Btn
-        onPress={() => {
-          navigation.navigate(DetailsInfoPage);
-        }}
-      >
-        <Text>카카오 네이버 로그인 페이지</Text>
-      </Btn>
+      <WithLocalSvg width={135} asset={FlintLogoSvg} />
     </Container>
   );
 };
