65a5c90590b477f5f00b5a60d53195707b9100ed
hyeseung <wjoo3@naver.com>
2023-08-15 19:53:29 +0900

    Feat/로컬에서 인증서 가져오는 로직 구현

--- components/signup/CertificatePage.jsx
+++ components/signup/CertificatePage.jsx
@@ -1,6 +1,6 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
+import * as DocumentPicker from 'expo-document-picker';
 
 import EmailVerificationPage2 from './EmailVerificationPage2';
 import SignupHeader from './SignupHeader';
@@ -8,34 +8,31 @@ import SignupHeader from './SignupHeader';
 const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
+  background: #fff;
 `;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Text = styled.Text`
-  color: white;
-  font-weight: 500;
-`;
-// FIXME: 페이지 작업 들어가면 해당 스타일링 삭제
-const Btn = styled.TouchableOpacity`
-  background-color: black;
-  padding: 10px 15px;
-  padding: 10px 15px;
-  margin: auto 0;
+const UploadBtn = styled.TouchableOpacity`
+  width: 100%;
+  height: 50px;
+  background-color: red;
 `;
 
 const CertificatePage = () => {
-  const navigation = useNavigation();
+  const [certificateURI, setCertificateURI] = useState('');
+
+  const selectCertificate = async () => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: ['image/*', 'application/pdf'],
+    });
+    if (result.canceled) console.log('canceled');
+    else {
+      setCertificateURI(result.assets[0].uri);
+      console.log(certificateURI);
+    }
+  };
   return (
     <Container>
-      <SignupHeader prevPage={EmailVerificationPage2} />
-      {/* FIXME: 버튼과 텍스트 부분 페이지 작업 들어가면 삭제 */}
-      <Btn
-        onPress={() => {
-          navigation.navigate(CertificatePage);
-        }}
-      >
-        <Text>증명서 첨부 페이지</Text>
-      </Btn>
+      <SignupHeader />
+      <UploadBtn title={'upload'} onPress={selectCertificate}></UploadBtn>
     </Container>
   );
 };
