5e4a090f7f321e649c8943592dbf0b676e753d51
hyeseung <wjoo3@naver.com>
2023-08-15 21:33:37 +0900

    Style/StartPage 스타일 수정

--- components/signup/StartPage.jsx
+++ components/signup/StartPage.jsx
@@ -34,10 +34,10 @@ const StartBtn = styled.TouchableOpacity`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  gap: 15;
+  gap: 15px;
   width: 100%;
   height: 55px;
-  border-radius: 8px;
+  border-radius: 5px;
   background-color: ${(props) => props.backgroundColor};
 `;
 const BtnText = styled.Text`
