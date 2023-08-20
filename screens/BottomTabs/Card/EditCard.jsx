2253f3980cb27dc0c2df1a33b63ce23c802c3656
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2023-08-20 18:47:23 +0900

    Feat/내명함 안에 카드 수정 버튼 작업 및 EditCard 스택 스크린 추가

---
+++ screens/BottomTabs/Card/EditCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View``;
+const Text = styled.Text``;
+
+const EditCard = () => {
+  return (
+    <Container>
+      <Text>EditCard</Text>
+    </Container>
+  );
+};
+
+export default EditCard;
--- screens/BottomTabs/Card/MyCard.jsx
+++ screens/BottomTabs/Card/MyCard.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
 
 const Container = styled.View``;
 const Text = styled.Text``;
+const Btn = styled.TouchableOpacity``;
 
 const MyCard = () => {
+  const navigation = useNavigation();
   return (
     <Container>
       <Text>MyCard</Text>
+      <Btn
+        onPress={() => {
+          navigation.navigate("Stack", { screen: "EditCard" });
+        }}
+      >
+        <Text>Edit Card</Text>
+      </Btn>
     </Container>
   );
 };
