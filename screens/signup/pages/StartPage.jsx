69746100a603bc0e042e6302963e6d1cb0721256
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2023-08-28 21:54:17 +0900

    Feat/#40 redux import (#41)
    * Chore/ react-redux, redux-toolkit 설치
    
    * Chore/App.js의 상위컴포넌트인 main.js 적용
    
    * Test/테스트용 store 및 slice 적용

--- screens/signup/pages/StartPage.jsx
+++ screens/signup/pages/StartPage.jsx
@@ -4,6 +4,7 @@ import { WithLocalSvg } from "react-native-svg";
 import { useNavigation } from "@react-navigation/native";
 import { login } from "@react-native-seoul/kakao-login";
 import NaverLogin from "@react-native-seoul/naver-login";
+import { useSelector } from "react-redux";
 
 import FlintLogoSvg from "../../../assets/images/flint_logo.svg";
 import KakaoLogoSvg from "../../../assets/images/kakao_logo.svg";
@@ -42,7 +43,13 @@ const BtnText = styled.Text`
 const StartPage = () => {
   const navigation = useNavigation();
   const [token, setToken] = useState(null);
-
+  //FIXME: 그냥 테스트 코드라 추후에 삭제필요.
+  /* user store에 올려놓은 토큰 값 받아와지는지 테스트 코드
+  const myToken = useSelector((state) => {
+    return state.user.token;
+  });
+  console.log(myToken)
+  */
   const signInWithKakao = async () => {
     try {
       const token = await login();
