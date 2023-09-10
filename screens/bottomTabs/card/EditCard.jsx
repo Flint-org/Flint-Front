8ec7d44875b6a5738d06562fc108070dc32171af
hyeseung <wjoo3@naver.com>
2023-09-10 20:21:37 +0900

    Feat/관심사 선택 영역 dropdown으로 변경

--- screens/bottomTabs/card/EditCard.jsx
+++ screens/bottomTabs/card/EditCard.jsx
@@ -6,6 +6,7 @@ import * as DocumentPicker from "expo-document-picker";
 import { LinearGradient } from "expo-linear-gradient";
 
 import BackArrowSvg from "../../../assets/images/back_arrow.svg";
+import ArrowDownSvg from "../../../assets/images/arrow_down.svg";
 import LogoSvg from "../../../assets/images/logo_symbol_white.svg";
 import PlusSvg from "../../../assets/images/plus.svg";
 import AlertModal from "../../../components/common/AlertModal";
@@ -219,19 +220,28 @@ const EditCard = () => {
           placeholder={"SNS 계정을 입력하세요."}
         />
         <InputTitle>관심사</InputTitle>
-        {interestTitle.map((title, index) => {
-          return (
-            <>
-              <InterestTitle>{title}</InterestTitle>
-              <InterestBtns
-                interests={interests}
-                type={index}
-                onCilckInterest={onCilckInterest}
-                setClickInterest={setClickInterest}
-              />
-            </>
-          );
-        })}
+        <InterestsWrap>
+          <ArrowIconWrap
+            onPress={() => setClickDropdown(!onClickDropdown)}
+            rotate={onClickDropdown}
+          >
+            <WithLocalSvg width={20} height={20} asset={ArrowDownSvg} />
+          </ArrowIconWrap>
+          {onClickDropdown &&
+            interestTitle.map((title, index) => {
+              return (
+                <>
+                  <InterestTitle>{title}</InterestTitle>
+                  <InterestBtns
+                    interests={interests}
+                    type={index}
+                    onCilckInterest={onCilckInterest}
+                    setClickInterest={setClickInterest}
+                  />
+                </>
+              );
+            })}
+        </InterestsWrap>
       </MainSection>
       {onClickSave && (
         <AlertModal
@@ -425,16 +435,24 @@ const Input = styled.TextInput`
   color: #000;
   font-weight: 500;
 `;
-const InterstBtnsWrap = styled.View`
+const InterestsWrap = styled.View`
+  display: flex;
+  gap: 20px;
   background-color: #f3f3f3;
   border-radius: 5px;
   margin-bottom: 6%;
-  padding: 4% 15px;
+  padding: 4% 5%;
+`;
+const ArrowIconWrap = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  transform: ${(props) => (props.rotate ? "rotate(180deg)" : "rotate(0deg)")};
 `;
 const InterestTitle = styled.Text`
-  margin-left: 12px;
-  margin-bottom: 5%;
-  font-size: 17px;
+  margin-left: 10px;
+  font-size: 18px;
   font-weight: 500;
 `;
 const InterestBtnWrap = styled.View`
@@ -442,7 +460,6 @@ const InterestBtnWrap = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
   gap: 15px;
-  margin-bottom: 5%;
 `;
 const InterestBtn = styled.TouchableOpacity`
   padding: 10px 15px;
