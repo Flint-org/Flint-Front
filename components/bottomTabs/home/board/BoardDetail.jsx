90068d644e2881a8b87a21a28264cfc48d09dea3
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2023-09-15 21:46:18 +0900

    Feat/글쓴이 정보 컴포넌트 적용 및 글 내용 표시

--- components/bottomTabs/home/board/BoardDetail.jsx
+++ components/bottomTabs/home/board/BoardDetail.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import styled from "styled-components/native";
 import BoardDetailHeader from "./BoardDetailHeader";
+import AuthorInfo from "./AuthorInfo";
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
 const Text = styled.Text``;
+const BoardWrap = styled.View``;
+const ContentWrap = styled.View`
+  padding: 0px 30px;
+`;
+const CommentWrap = styled.View``;
+const Title = styled.Text`
+  font-weight: 700;
+  font-size: 18px;
+  margin-bottom: 15px;
+`;
+const Content = styled.Text`
+  font-weight: 500;
+  font-size: 16px;
+`;
 
 const BoardDetail = ({ route }) => {
   const { postData } = route.params;
-
+  //console.log(postData);
   return (
     <Container>
       <BoardDetailHeader title={postData.type} subTitle={postData.type} />
-      <Text>BoardDetail</Text>
+      <BoardWrap>
+        <AuthorInfo postData={postData} />
+        <ContentWrap>
+          <Title>{postData.title}</Title>
+          <Content>{postData.content}</Content>
+        </ContentWrap>
+        <Content></Content>
+      </BoardWrap>
+      <CommentWrap></CommentWrap>
     </Container>
   );
 };
 
-const AuthorInfo = () => {};
 export default BoardDetail;
