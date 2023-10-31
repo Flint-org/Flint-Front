4bbae843218afcdc63988c29a413bd7eaaff7d83
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2023-10-31 10:58:01 +0900

    Feat/일반게시판 목록 조회

--- screens/bottomTabs/home/GeneralBoard.jsx
+++ screens/bottomTabs/home/GeneralBoard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import BoardHeader from "../../../components/bottomTabs/home/board/BoardHeader";
@@ -7,10 +7,24 @@ import FreshmanBoard from "./generalBoard/FreshmanBoard";
 import PromotionBoard from "./generalBoard/PromotionBoard";
 import DateBoard from "./generalBoard/DateBoard";
 import PopularBoard from "./generalBoard/PopularBoard";
+import { useQuery } from "react-query";
+import { communityAPI } from "../../../api";
+import Loader from "../../../components/common/Loader";
 
 const Tab = createMaterialTopTabNavigator();
 
 const GeneralBoard = () => {
+  const { isLoading, data: boardObj } = useQuery(
+    "generalBoardList",
+    communityAPI.generalBoardList
+  );
+
+  if (isLoading) {
+    return <Loader visible={isLoading} />;
+  }
+  const {
+    data: { data: boardData },
+  } = boardObj;
   return (
     <>
       <BoardHeader title={"일반게시판"} />
@@ -18,18 +32,38 @@ const GeneralBoard = () => {
         screenOptions={{
           swipeEnabled: false,
           tabBarItemStyle: { width: 145 },
-          tabBarLabelStyle: { fontSize: 18, fontWeight: 600, marginBottom: 8 },
+          tabBarLabelStyle: { fontSize: 17, fontWeight: 600, marginBottom: 5 },
           tabBarIndicatorStyle: {
             backgroundColor: "#FF9810",
           },
           tabBarScrollEnabled: true,
         }}
       >
-        <Tab.Screen name="자유게시판" component={FreeBoard} />
-        <Tab.Screen name="새내기게시판" component={FreshmanBoard} />
-        <Tab.Screen name="홍보게시판" component={PromotionBoard} />
-        <Tab.Screen name="연애게시판" component={DateBoard} />
-        <Tab.Screen name="인기게시판" component={PopularBoard} />
+        <Tab.Screen
+          name={boardData[0].boardName}
+          component={FreeBoard}
+          initialParams={{ boardId: boardData[0].boardId }}
+        />
+        <Tab.Screen
+          name={boardData[1].boardName}
+          component={FreshmanBoard}
+          initialParams={{ boardId: boardData[1].boardId }}
+        />
+        <Tab.Screen
+          name={boardData[2].boardName}
+          component={PromotionBoard}
+          initialParams={{ boardId: boardData[2].boardId }}
+        />
+        <Tab.Screen
+          name={boardData[3].boardName}
+          component={DateBoard}
+          initialParams={{ boardId: boardData[3].boardId }}
+        />
+        <Tab.Screen
+          name={boardData[4].boardName}
+          component={PopularBoard}
+          initialParams={{ boardId: boardData[4].boardId }}
+        />
       </Tab.Navigator>
     </>
   );
